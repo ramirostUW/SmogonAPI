0f6d8bf0247aed85c5c4a3a6ee02bb62e55aa11d
Ramiro Steinmann Petrasso <ramirost@live.com>
2023-03-14 08:57:28 -0700

    smogon endpoints and tests (besides sprites)

--- smogonapi/tests/test_smogon_endpoints.py
+++ smogonapi/tests/test_smogon_endpoints.py
@@ -7,7 +7,9 @@ TestSmogon_Endpoints(unittest.TestCase)
     Performs a series of different tests on the smogon_endpoints module
 """
 import unittest
+from fastapi.testclient import TestClient
 
+from smogonapi.main import myApp
 from smogonapi.submodules import smogon_endpoints
 
 class TestSmogonEndpoints(unittest.TestCase):
@@ -83,6 +85,99 @@ class TestSmogonEndpoints(unittest.TestCase):
         """
         with self.assertRaises(ValueError):
             smogon_endpoints.get_top_pokemon(['atk', 'spe'], 'rby')
+
+    def test_smoke_get_smogon(self):
+        """
+        This smoke test ensures the GetSmogonData endpoint
+        returns a valid response.
+        """
+        client = TestClient(myApp)
+        response = client.get("/GetSmogonData/ss/Charizard")
+        assert response.status_code == 200
+
+        pokemon = response.json()
+        assert "languages" in pokemon.keys()
+        assert "learnset" in pokemon.keys()
+        assert "strategies" in pokemon.keys()
+
+    def test_pokedata_invalid_gen(self):
+        """
+        This smoke test ensures the GetSmogonData endpoint
+        returns a ValueError if the gen is invalid.
+        """
+        client = TestClient(myApp)
+        response = client.get("/GetSmogonData/badGen/Charizard")
+        assert response.status_code == 200
+        assert response.json()['errorType'] == "ValueError"
+
+    def test_pokedata_invalid_poke(self):
+        """
+        This smoke test ensures the GetSmogonData endpoint
+        returns ValueError if the Pokemon name is invalid.
+        """
+        client = TestClient(myApp)
+        response = client.get("/GetSmogonData/ss/Carizard")
+        assert response.status_code == 200
+        assert response.json()['errorType'] == "ValueError"
+
+    def test_smoke_get_tierdata(self):
+        """
+        This smoke test ensures the GetPokemonByGenAndTier endpoint
+        returns a valid response.
+        """
+        client = TestClient(myApp)
+        response = client.get("/GetPokemonByGenAndTier/ss/OU")
+        assert response.status_code == 200
+
+        pokemon_list = response.json()
+        for pokemon in pokemon_list:
+            assert "name" in pokemon.keys()
+            assert "types" in pokemon.keys()
+            assert "abilities" in pokemon.keys()
+
+    def test_tierdata_invalid_gen(self):
+        """
+        This smoke test ensures the GetPokemonByGenAndTier endpoint
+        returns a ValueError if the gen is invalid.
+        """
+        client = TestClient(myApp)
+        response = client.get("/GetPokemonByGenAndTier/badGen/OU")
+        assert response.status_code == 200
+        assert response.json()['errorType'] == "ValueError"
+
+    def test_tierdata_invalid_tier(self):
+        """
+        This smoke test ensures the GetPokemonByGenAndTier endpoint
+        returns ValueError if the Pokemon name is invalid.
+        """
+        client = TestClient(myApp)
+        response = client.get("/GetPokemonByGenAndTier/ss/OP")
+        assert response.status_code == 200
+        assert response.json()['errorType'] == "ValueError"
+    
+    def test_smoke_get_items(self):
+        """
+        This smoke test ensures the GetItems endpoint
+        returns a valid response.
+        """
+        client = TestClient(myApp)
+        response = client.get("/GetItems/ss/")
+        assert response.status_code == 200
+
+        items_list = response.json()
+        for item in items_list:
+            assert "name" in item.keys()
+            assert "description" in item.keys()
+
+    def test_items_invalid_gen(self):
+        """
+        This smoke test ensures the GetItems endpoint
+        returns a ValueError if the gen is invalid.
+        """
+        client = TestClient(myApp)
+        response = client.get("/GetItems/badGen")
+        assert response.status_code == 200
+        assert response.json()['errorType'] == "ValueError"
 
 if __name__ == '__main__':
     unittest.main()
