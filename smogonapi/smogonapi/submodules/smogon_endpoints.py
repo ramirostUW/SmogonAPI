2c46f19583a17be4c5cbdecf3ca5526cd72d6630
marquesjchacon <mrqs97@uw.edu>
2023-03-13 22:34:17 -0700

    pleeeease work

--- smogonapi/smogonapi/submodules/smogon_endpoints.py
+++ smogonapi/smogonapi/submodules/smogon_endpoints.py
@@ -81,7 +81,7 @@ def get_pokemon_data():
     return json.loads(script_insides.replace("dexSettings = ", "").strip())['injectRpcs'][1][1]
 
 @router.get("/getTopPokemon")
-async def get_top_pokemon(stats: list[str] = Query(), gen: str = Query()):
+def get_top_pokemon(stats: list = Query(), gen: str = Query()):
     """
     Scrapes the HTML from the Smogon Dex for the given generation. Particularly, it scrapes all
     eligible pokemon in the generation (along with their stats), compacts it into dataframe object,
